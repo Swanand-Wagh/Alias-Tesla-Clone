16ef4ecdea065921ba2fa39804e1bd11c5794445
Swanand-Wagh <swanandwagh7@gmail.com>
2021-09-25 10:49:25 +0530

    Header Mileage Section

--- components/CarItem/CarItem.js
+++ components/CarItem/CarItem.js
@@ -1,16 +1,47 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCog, faToolbox } from "@fortawesome/free-solid-svg-icons";
 import styles from "./styles";
 
 const CarItem = () => {
   return (
     <>
       <View style={styles.carContainer}>
+        <ImageBackground
+          source={require("../../assets/background.png")}
+          style={styles.backgroundImage}
+        />
+
         {/* Header */}
         <View style={styles.header}>
-          <Text>Hey Hey</Text>
-          <Text>Hey Hey</Text>
-          <Text>Hey Hey</Text>
+          <TouchableOpacity>
+            <FontAwesomeIcon style={styles.icon} icon={faCog} size={24} />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>Hey Hey</Text>
+          <TouchableOpacity>
+            <FontAwesomeIcon style={styles.icon} icon={faToolbox} size={24} />
+          </TouchableOpacity>
+        </View>
+
+        {/* Mileage */}
+        <View style={styles.batterySection}>
+          <Image
+            source={require("../../assets/battery.png")}
+            style={styles.batteryImg}
+          />
+          <Text style={styles.batteryText}>150 mi</Text>
+        </View>
+
+        {/* Status */}
+        <View style={styles.status}>
+          <Text style={styles.statusText}>Parked </Text>
         </View>
       </View>
     </>
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -10,9 +10,48 @@ const styles = StyleSheet.create({
     marginTop: 50,
     marginLeft: 20,
     marginRight: 20,
+    marginBottom: 10,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+  },
+  headerTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon: {
+    color: "white",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+  batterySection: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  batteryImg: {
+    height: 26,
+    width: 70,
+  },
+  batteryText: {
+    color: "white",
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  status: {
+    alignItems: "center",
+  },
+  statusText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
